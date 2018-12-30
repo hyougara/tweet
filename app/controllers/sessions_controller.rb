f6861a22e2003ec277bfd058e9b858e2452e0a57
前田優樹 <orzazous2@gmail.com>
2018-12-30 23:19:36 +0900

    クッキーによるログイン昨日実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
   end
   
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-      if @user && @user.authenticate(params[:session][:password])
-        log_in @user
-        redirect_to @user ,notice:"ログインしました"
+    user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        log_in user
+        params[:session][:remember_me] == "1" ? remember(user):forget(user)
+        redirect_to user ,notice:"ログインしました"
       else
         flash.now[:danger] = 'emailかpasswordが間違えています'
         render :new 
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 
